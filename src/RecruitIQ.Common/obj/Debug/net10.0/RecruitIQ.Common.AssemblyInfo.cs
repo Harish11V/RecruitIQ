50fcb89c3f8898e56f6c8f3ed68181431029c5a1
Harish V <harishrojers@gmail.com>
2026-06-25 19:34:42 +0530

    feat(solution): complete clean architecture project structure

--- src/RecruitIQ.Common/obj/Debug/net10.0/RecruitIQ.Common.AssemblyInfo.cs
+++ src/RecruitIQ.Common/obj/Debug/net10.0/RecruitIQ.Common.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RecruitIQ.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+381e4b2704ac33419ac2f1b171c5ae1d0a64abb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("RecruitIQ.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RecruitIQ.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
